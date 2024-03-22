a1363878855736a963807f25f43fccbfb9a03695
unknown <jordan.hiphop21@gmail.com>
2024-03-22 10:41:18 +0700

    week 5

---
+++ minggu_5_api/README.md
@@ -0,0 +1,3 @@
+# minggu_5_api
+
+A new Flutter project.
---
+++ minggu_5_api/android/app/src/main/kotlin/com/example/minggu_5_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.minggu_5_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
